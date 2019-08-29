257611e7211fd9b270c15ae2bebcd323e1b7540c
KHong <albtrossfacefall@gmail.com>
2019-08-29 02:25:12 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,31 @@
 class Customer
-  
+   attr_accessor :name, :age
+   @@all=[]
+
+   def initialize (name,age)
+      @name=name
+      @age=age
+      @@all.push(self)
+   end
+
+   def self.all
+      @@all
+   end
+   
+   def new_meal(waiter,total,tip=0)
+      Meal.new(waiter,self,total,tip)
+   end
+
+   def meals
+      Meal.all.select do |m|
+         m.customer==self
+      end
+   end
+
+   def waiters
+       meals.map do |m|
+         m.waiter
+      end
+   end
+
 end
